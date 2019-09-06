881fb71c6e389573e35404b438d7c75f67aa756c
Chris Smith <cs24@sanger.ac.uk>
2019-09-06 16:03:34 +0100

    Change application defaults

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ Bundler.require(*Rails.groups)
 
 module Labwhere
   class Application < Rails::Application
-    config.load_defaults 5.1
+    config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
